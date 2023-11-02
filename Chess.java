9bc2e754e8293a9dac0fe0bb3e6eb40e1ccb92cc
f10983ck <cyrus.knopf@student.manchester.ac.uk>
2023-11-02 23:14:05 +0000

    Added primitive move function to PawnBoard

--- Chess.java
+++ Chess.java
@@ -2,19 +2,8 @@ public class Chess {
     public static void main(String[] args) {
         PawnBoard p = new PawnBoard();
         p.initialiseBoard(false);
-
-        Bitboard w = new Bitboard();
-        w.board = 1L << (1 + 1*8);
-        System.out.println("\n w:\n");
-        System.out.println(w);
-
-        p.board = p.board ^ w.board;
-        System.out.println("\n XOR'd pawnboard:\n");
+        p.primitiveMove(5, 1);
         System.out.println(p);
-        w.board = w.board << 8;
-        p.board |= w.board;
-        System.out.println(p);
-
 
     }
 }
