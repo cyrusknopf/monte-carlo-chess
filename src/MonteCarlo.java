7a1a786d9b3d19e0fe823029f920fa3df63b885d
Cyrus Knopf <cyrus.knopf@student.manchester.ac.uk>
2023-11-18 17:08:18 +0000

    Added monte carlo agent

---
+++ src/MonteCarlo.java
@@ -0,0 +1,21 @@
+public class MonteCarlo {
+    
+    public MonteCarlo() {
+
+    }
+
+
+    private void explore() {
+
+    }
+    private void rollout() {
+
+    }
+    private void backprop() {
+        
+    }
+
+
+
+
+}
