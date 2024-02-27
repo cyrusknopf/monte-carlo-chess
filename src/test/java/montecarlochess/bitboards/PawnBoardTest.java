8379bbaab1e726e710a12d686d0ca1a0e6f3fdf8
Cyrus Knopf <cyrus.knopf@student.manchester.ac.uk>
2024-02-27 10:29:00 +0000

    PawnBoard Tests

--- src/test/java/montecarlochess/bitboards/PawnBoardTest.java
+++ src/test/java/montecarlochess/bitboards/PawnBoardTest.java
@@ -1,5 +1,32 @@
-package test;
+package montecarlochess.bitboards;
+
+import montecarlochess.Chess;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+
 
 public class PawnBoardTest {
-    
+
+    private Chess game;
+
+    @BeforeEach
+    public void setUp() {
+        game = new Chess(); // Assuming Chess class has a default constructor
+    }
+
+    @Test
+    public void testInitialiseBoardWhite() {
+        PawnBoard whiteBoard = new PawnBoard(game, true);
+        long expected = 0x000000000000FF00L;
+        Assertions.assertEquals(expected, whiteBoard.initialiseBoard());
+    }
+
+    @Test
+    public void testInitialiseBoardBlack() {
+        PawnBoard blackBoard = new PawnBoard(game, false);
+        long expected = Long.reverse(0x000000000000FF00L);
+        Assertions.assertEquals(expected, blackBoard.initialiseBoard());
+    }
 }
